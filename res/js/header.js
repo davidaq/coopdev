29aba2a0ec733020ca27e628d014956db11fa97b
DavidAQ <aq@num16.com>
2014-09-21 18:46:37 +0800

    headroom

--- res/js/header.js
+++ res/js/header.js
@@ -1,7 +1,10 @@
 $(function() {
+    new Headroom($('.header')[0], {
+      scroll: 70,
+      tolerance: 5
+    }).init();
     $('.header a').tooltip({
         toggle: 'tooltip',
         placement: 'bottom'
     });
-    
 });

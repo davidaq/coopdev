8bafdadc9ec0c3fcd32e61bca10ba86f759d5467
DavidAQ <anqin@ccme.cc>
2015-03-19 20:26:19 +0800

    x

--- chron.sh
+++ chron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    curl 'http://localhost/coopdev/dev?chron' 2>/dev/null | while read x; do echo $x; curl "$x" > /dev/null 2>&1; done
+    curl 'http://dev.ccmelive.com/dev?chron' 2>/dev/null | while read x; do echo $x; curl "$x" > /dev/null 2>&1; done
     sleep 2
 done
